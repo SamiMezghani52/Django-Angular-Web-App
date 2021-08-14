b759a7e12639d6c34863d5f29a21a797e134dab9
SamiMezghani52 <sami.mezghenni@gmail.com>
2021-08-14 23:01:53 +0100

    autoCommit 20210814.230153

--- WebAppAPI/Employee/views.py
+++ WebAppAPI/Employee/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from .models import Department, Employess
+from .serializers import DepartmentSerializer, EmployeeSerializer
 
 # Create your views here.
