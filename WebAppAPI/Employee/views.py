c164ae6abaf69802169476c1429069ce4c1fdb5f
SamiMezghani52 <sami.mezghenni@gmail.com>
2021-08-14 23:18:10 +0100

    autoCommit 20210814.231810

--- WebAppAPI/Employee/views.py
+++ WebAppAPI/Employee/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
 
 from .models import Department, Employess
 from .serializers import DepartmentSerializer, EmployeeSerializer
 
 # Create your views here.
+@csrf_exempt
+def DepartmentApi(request, id=0):
+    if request.method == 'GEt':
+        departments = Department.objects.all()
+        departments_serializer = DepartmentSerializer(departments, many=True)
